c012cc95cb01f3c5639c44e9e09b68227820efab
roy-santos <roysantos112@gmail.com>
2023-06-28 14:57:27 -0700

    Add user input and store in array.

--- main.go
+++ main.go
@@ -1,21 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
 	fmt.Println("Welcome to", conferenceName, "booking application!")
 
 	if remainingTickets == 0 {
 		fmt.Println("The conference is sold out")
 	} else {
-		fmt.Printf("We have a total of %v tickets and %v tickets  are"+
+		fmt.Printf("We have a total of %v tickets and %v tickets  are "+
 			"still available.\n", conferenceTickets, remainingTickets)
-
 	}
 
 	fmt.Println("Get your tickets here to attend.")
+
+	var bookings [50]string
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	// ask user for their name
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
 }
